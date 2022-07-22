a2e6a29fb5cd4a779e1d47c498443820f450b587
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-22 12:24:30 -0500

    set up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+server.use((err, req, res, next) =>{
+    res.status(err.status || 500).json({ message: err.message})
+})
+
+module.exports = server
