37e1d132a44b3208b02acaf2f8709accb29c3dbc
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-22 21:45:21 -0500

    set up models and routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,13 +1,22 @@
-// build your `/api/projects` router here
 const express = require('express')
-const helpers = require('./model')
+const Project = require('./model')
 
 const router = express.Router();
 
-router.get('/projects', (req, res, next => {
-    helpers.getProjects()
+router.get('/', (req, res, next) => {
+    Project.getProjects()
         .then(projects => {
             res.status(200).json(projects);
         })
         .catch(next)
-}));
+});
+
+router.use((err,req, res, next) => {
+    res.status(500).json({
+        customMessage: 'something went wrong in the projects router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
