2c17b15c66285fa924694041da2fa771ddc4230f
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-22 22:01:53 -0500

    wip

--- data/migrations/20220722181415_project_resources.js
+++ data/migrations/20220722181415_project_resources.js
@@ -3,18 +3,18 @@ exports.up = function(knex) {
         .createTable('projects', tbl=> {
             tbl.increments('project_id');
             tbl.text('project_name', 128).notNullable();
-            tbl.text('project_description', 128);
+            tbl.text('project_description', 200);
             tbl.boolean('project_completed').defaultTo(0)
         })
         .createTable('resources', tbl=> {
             tbl.increments('resource_id');
             tbl.text('project_name', 128).unique().notNullable();
-            tbl.text('project_description', 128)
+            tbl.text('project_description', 200)
         }) 
         .createTable('tasks', tbl => {
             tbl.increments('task_id');
             tbl.text('task_description', 128).notNullable();
-            tbl.text('task_notes', 128);
+            tbl.text('task_notes', 200);
             tbl.boolean('task_completed').defaultTo(0)
             tbl.integer('project_id')
                 .unsigned()
